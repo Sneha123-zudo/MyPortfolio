5d5f8c16c38d35d5c72adfd7c2fb95531b2d384a
Sneha123-zudo <snehamishrasm9@gmail.com>
2025-08-14 01:13:30 +0530

    education

---
+++ script.js
@@ -0,0 +1,14 @@
+function switchTab(evt, tabName) {
+    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
+    document.querySelectorAll('.tab').forEach(button => button.classList.remove('active', 'green-active', 'yellow-active'));
+
+    document.getElementById(tabName).classList.add('active');
+
+    if (tabName === 'education') {
+        evt.currentTarget.classList.add('active');
+    } else if (tabName === 'events') {
+        evt.currentTarget.classList.add('green-active');
+    } else if (tabName === 'achievements') {
+        evt.currentTarget.classList.add('yellow-active');
+    }
+}
